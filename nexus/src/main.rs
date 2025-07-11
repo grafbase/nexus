99492646a057bdf3160ff0049764ad0cc458a95b
Julius de Bruijn <julius@nauk.io>
2025-07-11 14:22:54 +0000

    chore: anyhow, tokio, taplo, fastrace, logforth

--- nexus/src/main.rs
+++ nexus/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use args::Args;
+use clap::Parser;
+
+mod args;
+mod logger;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    logger::init(&args);
+
+    {
+        log::error!("Hello error!");
+        log::warn!("Hello warn!");
+        log::info!(foo = "bar"; "Hello info!");
+        log::debug!("Hello debug!");
+        log::trace!("Hello trace!");
+    }
+
+    fastrace::flush();
+
+    Ok(())
 }
